2e7b6f5acbf43ad1ac9fb48f04ac8aa8363c0f9a
Nick Reyno <nickrenoe@gmail.com>
2020-03-19 17:31:56 -0400

    loading icons

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 app = {};
-dynamicOffset = 0.05 * window.innerHeight;
+// let dynamicOffset = 0.05 * window.innerHeight;
 
 app.anchorNav = function(linkClicked) {
 	let anchor = $(linkClicked).attr("href");
 	console.log(anchor);
 	$.smoothScroll({
 		beforeScroll: function() {
-			app.preScroll(linkClicked);
+			app.preScroll();
 		},
 		afterScroll: function() {
-			app.postScroll(linkClicked);
+			app.postScroll();
 		},
 		scrollElement: $("main"),
 		scrollTarget: anchor
@@ -17,21 +17,40 @@ app.anchorNav = function(linkClicked) {
 	return false;
 };
 
-app.preScroll = function(linkClicked){
-    $("main").css("scroll-snap-type", "none");
-    $('a').parent().removeClass('activeNav')
-    $(linkClicked).parent().addClass('activeNav');
-}
+app.preScroll = function() {
+	$("main").css("scroll-snap-type", "none");
+};
 
-app.postScroll = function(){
-    $("main").css("scroll-snap-type", "y mandatory");
-    
-}
+app.postScroll = function() {
+	$("main").css("scroll-snap-type", "y mandatory");
+};
+
+app.navActive = function() {
+	let scrolled = $("main").scrollTop();
+	$("li").removeClass("activeNav");
+	if (scrolled > 3.5 * window.innerHeight) {
+		$(".nav5").addClass("activeNav");
+	} else if (scrolled > 2.5 * window.innerHeight) {
+		$(".nav4").addClass("activeNav");
+	} else if (scrolled > 1.5 * window.innerHeight) {
+		$(".nav3").addClass("activeNav");
+	} else if (scrolled > 0.5 * window.innerHeight) {
+		$(".nav2").addClass("activeNav");
+	} else if (scrolled >= 0) {
+		$(".nav1").addClass("activeNav");
+	}
+};
 
 app.init = function() {
 	$("a").on("click", function(e) {
 		e.preventDefault();
 		app.anchorNav(this);
+	});
+
+	$("main").on("scroll", function() {
+		setTimeout(() => {
+			app.navActive();
+		}, 300);
 	});
 };
 
